2c7b4f57a709e6af9caf1ffd7858368e3e8e051f
Jimmy <edygatojr@live.com>
2022-06-13 22:19:36 -0300

    Iniciando o Projeto - Inserindo as primeiras variaveis e mostrando em tela via console.

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Boas Vindas ao seu Banco, ByteBank");
+
+string titular = "Edmilson Gato Jr";
+string conta = "10123-x";
+int numero_agencia = 23;
+string nome_agencia = "Agencia Central";
+double saldo = 93.50;
+
+
+Console.WriteLine("Titula: " + titular);
+Console.WriteLine("Conta: " + conta);
+Console.WriteLine("Número da Agencia: " + numero_agencia);
+Console.WriteLine("Nome da Agencia: " + nome_agencia);
+Console.WriteLine("Saldo: R$" + saldo);
+
+Console.ReadKey();
