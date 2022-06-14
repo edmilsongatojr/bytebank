e7bbfa0f997a603a22d95df65b0a318a2b00aa93
Jimmy <edygatojr@live.com>
2022-06-13 22:33:59 -0300

    Entendo a orientação a objetos | OO

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,17 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using bytebank;
+
 Console.WriteLine("Boas Vindas ao seu Banco, ByteBank");
 
-string titular = "Edmilson Gato Jr";
-string conta = "10123-x";
-int numero_agencia = 23;
-string nome_agencia = "Agencia Central";
-double saldo = 93.50;
-
-
-Console.WriteLine("Titula: " + titular);
-Console.WriteLine("Conta: " + conta);
-Console.WriteLine("Número da Agencia: " + numero_agencia);
-Console.WriteLine("Nome da Agencia: " + nome_agencia);
-Console.WriteLine("Saldo: R$" + saldo);
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "Edmilson Gato Jr";
+conta1.conta = "10123-X";
+conta1.numero_agencia = 23;
+conta1.nome_agencia = "Agencia Central";
+conta1.saldo = 100;
+    
+Console.WriteLine("Titula: " + conta1.titular);
+Console.WriteLine("Conta: " + conta1.conta);
+Console.WriteLine("Número da Agencia: " + conta1.numero_agencia);
+Console.WriteLine("Nome da Agencia: " + conta1.nome_agencia);
+Console.WriteLine("Saldo: R$" + conta1.saldo);
 
 Console.ReadKey();
