29b7f058ac5fc75fb52e3ab7005e7497d9a47203
Jimmy <edygatojr@live.com>
2022-06-13 23:26:12 -0300

    Criação dos métodos Sacar e Depositar na classe ContaCorrente e inicio da separação das Classes ContaCorrente / Clientes. Junto com a criação da Classe Clientes.

---
+++ bytebank/Cliente.cs
@@ -0,0 +1,6 @@
+﻿namespace bytebank
+{
+    public  class Cliente
+    {
+    }
+}
--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -2,10 +2,53 @@
 {
     public class ContaCorrente
     {
-        public string titular;
+        public string titular_nome;
+        public string titular_CPF;
+        public string titular_Profissao;
+
         public string conta;
         public int numero_agencia;
         public string nome_agencia;
         public double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor || valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                return true;
+            }
+        }
+
+        public bool Depositar(double valor)
+        {
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo += valor;
+                return true;
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente xDestino)
+        {
+            if (saldo < valor || valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                xDestino.saldo += valor;
+                return true;
+            }
+        }
     }
 }
