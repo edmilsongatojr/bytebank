379976e9e8bebecedb5689f7135492232ae54a2e
Edmilson Gato Junior <edygatojr@live.com>
2022-07-04 23:41:14 -0300

    Ajustes

---
+++ bytebank/LetiorDeArquivo.cs
@@ -0,0 +1,25 @@
+﻿namespace bytebank
+{
+    public class LetiorDeArquivo
+    {
+
+        public string Arquivo { get; }
+
+        public LetiorDeArquivo(string arquivo)
+        {
+            Arquivo = arquivo;
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+
+        public  string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha...");
+            return "Linha do Arquivo";
+        }
+
+        public void Fechar()
+        {
+            Console.WriteLine("Fechado Arquivo.");
+        }
+    }
+}
